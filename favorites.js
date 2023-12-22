37367acad9cb5348611e5c0cda574509766476f8
George Bilias <georgebiliasgr@gmail.com>
2023-12-22 18:18:21 +0200

    gg

--- favorites.js
+++ favorites.js
@@ -1,55 +1,12 @@
-// Sample favorites handling script
+function toggleImage(id) {
+    const button = document.getElementById(id);
 
-// Function to toggle favorite status
-function toggleFavorite(listingId) {
-    const favorites = getFavorites(); // Get current favorites from storage
-    const index = favorites.indexOf(listingId);
-
-    if (index !== -1) {
-        // Remove from favorites if already present
-        favorites.splice(index, 1);
-    } else {
-        // Add to favorites if not present
-        favorites.push(listingId);
-    }
-
-    saveFavorites(favorites); // Save updated favorites to storage
-    updateHeartColor(listingId); // Update heart color
-}
-
-// Function to update heart color
-function updateHeartColor(listingId) {
-    const heart = document.querySelector(`[data-id="${listingId}"]`);
-    if (heart) {
-        const isFavorited = getFavorites().includes(listingId);
-        heart.classList.toggle('favorited', isFavorited);
-    }
-}
-
-// Function to get favorites from local storage
-function getFavorites() {
-    const favoritesJSON = localStorage.getItem('favorites');
-    return favoritesJSON ? JSON.parse(favoritesJSON) : [];
-}
-
-// Function to save favorites to local storage
-function saveFavorites(favorites) {
-    const favoritesJSON = JSON.stringify(favorites);
-    localStorage.setItem('favorites', favoritesJSON);
-}
-
-let isImage1 = true; // Flag to track the current image state
-
-function toggleImage() {
-    const button = document.querySelector('.heart');
-
-    // Toggle between two images based on the flag
-    if (isImage1) {
-        button.style.backgroundImage = 'url("photos/red-heart.png")';
-    } else {
+    // Toggle between two images based on the current background image
+    if (button.style.backgroundImage == 'url("photos/red-heart.png")') {
         button.style.backgroundImage = 'url("photos/heart.png")';
+    } else {
+        button.style.backgroundImage = 'url("photos/red-heart.png")';
     }
 
-    // Toggle the flag for the next click
-    isImage1 = !isImage1;
+    
 }
