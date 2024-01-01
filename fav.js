f628a321085d16b2e780749a0f4a0b7f270d083d
George Bilias <georgebiliasgr@gmail.com>
2024-01-01 13:41:09 +0200

    gg

--- fav.js
+++ fav.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    getFavorites();
 async function getFavorites() {
     const sessionId = sessionStorage.getItem('sessionId'); // Get sessionId from sessionStorage
     const username = sessionStorage.getItem('username'); // Get username from sessionStorage
@@ -12,47 +14,24 @@ async function getFavorites() {
             }),
         });
     if (response.ok) {
-        const favorites = await response.json();
-        console.log(`User's favorite ads:`, favorites);
+        const listings = await response.json();
+        console.log(`User's favorite ads:`, listings);
+        const listingsContainer = document.getElementById("listings-container");
+        // Get the template from the HTML document
+        const source = document.getElementById('listing-template').innerHTML;
 
-        const favoritesContainer = document.getElementById('listings-container');
-        const favContainer = document.createElement('div');
-        favContainer.classList.add('listings');
+        // Compile the template
+        const template = Handlebars.compile(source);
+
         
-        // Create the ol element
-        const favList = document.createElement('ol');
-        favList.classList.add('grid');
-        
-        favorites.forEach((ad) => {
-            const adContainer = document.createElement('li');
-            adContainer.classList.add('listing');
-            adContainer.innerHTML = `
-                <h3>${ad.title}</h3>
-                <ul>
-                    <li>
-                        <ul>
-                            <li class="img">
-                                <img src="${ad.image_url}" alt="" height="200px">
-                            </li>
-                        </ul>
-                    </li>
-                    <li>${ad.description}</li>
-                    <div class="cost">
-                        <li class="cost">${ad.cost} €</li>
-                    </div>
-                </ul>
-            `;
-        
-            // Append the adContainer to the favList
-            favList.appendChild(adContainer);
-        });
-        
-        // Append the favList to the favContainer
-        favContainer.appendChild(favList);
-        
-        favoritesContainer.appendChild(favContainer);
+        // Generate the HTML for this ad using the template
+        const html = template({listings: listings});
+
+        // Append the generated HTML to the favList
+        listingsContainer.innerHTML = html;
 
     } else {
         console.error('Failed to fetch user favorites');
     }
 }
+});
